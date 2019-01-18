f8279e1fb305ca3df37eb09a88a6d94c50086c08
SirawichDev <sirawit0676@gmail.com>
2019-01-18 10:33:42 +0700

    use context and new parser in mongo

--- backEnd/server.js
+++ backEnd/server.js
@@ -2,46 +2,22 @@ const { ApolloServer, gql } = require('apollo-server');
 
 const mongoose = require('mongoose');
 require('dotenv').config({ path: '.env' });
-
+const User = require('./models/User');
+const Article = require('./models/Article');
 mongoose
-    .connect(process.env.M_DB)
+    .connect(
+        process.env.M_DB,
+        { useNewUrlParser: true }
+    )
     .then(() => console.log('Mongo Connected'))
     .catch(err => console.log(err));
 
-const Me = [
-    {
-        name: 'Sirawich',
-        haveMoney: true
-    }
-];
-const typeDefs = gql`
-    type Me {
-        name: String
-        haveMoney: Boolean
-    }
-    type Query {
-        getMe: [Me]
-    }
-    type Mutation {
-        moneyLost(name: String, haveMoney: Boolean): Me
-    }
-`;
-
-const resolvers = {
-    Query: {
-        getMe: () => Me
-    },
-    Mutation: {
-        moneyLost: (_, args) => {
-            const donthaveit = { name: args.name, haveMoney: args.haveMoney };
-            Me.push(donthaveit);
-            return donthaveit;
-        }
-    }
-};
 const server = new ApolloServer({
     typeDefs,
-    resolvers
+    context: {
+        User,
+        Article
+    }
 });
 server.listen().then(({ url }) => {
     console.log(`Listen on port ${url}`);
