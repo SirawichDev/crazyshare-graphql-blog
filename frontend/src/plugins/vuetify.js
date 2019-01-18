d5fad8e240d4bb614d2740e00e9cd900ce361f06
SirawichDev <sirawit0676@gmail.com>
2019-01-18 23:38:01 +0700

    added vuetify css

--- frontend/src/plugins/vuetify.js
+++ frontend/src/plugins/vuetify.js
@@ -1,7 +1,16 @@
-import Vue from 'vue'
-import Vuetify from 'vuetify/lib'
-import 'vuetify/src/stylus/app.styl'
+import Vue from 'vue';
+import Vuetify from 'vuetify/lib';
+import 'vuetify/src/stylus/app.styl';
 
 Vue.use(Vuetify, {
-  iconfont: 'md',
-})
+    iconfont: 'md',
+    theme: {
+        primary: '#E93148',
+        secondary: '#cddc39',
+        accent: '#009688',
+        error: '#e91e63',
+        warning: '#ffeb3b',
+        info: '#2196f3',
+        success: '#ff5722'
+    }
+});
