486b6ba7f433ff14f68c42751f1c59c2c2e398d9
SirawichDev <sirawit0676@gmail.com>
2019-01-18 14:15:17 +0700

    playing vuetify

---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container>
+      <v-content>
+        <h2>Welcome to CrazyDevShare</h2>
+      </v-content>
+  </v-container>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
