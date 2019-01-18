c443b315c8bcdab317ec432894eebe267b66cb9d
SirawichDev <sirawit0676@gmail.com>
2019-01-18 23:36:50 +0700

    add Home page

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -1,15 +1,14 @@
 <template>
   <v-container>
-      <v-content>
-        <h2>Welcome to CrazyDevShare</h2>
-      </v-content>
+    <v-content>
+      <h2>Welcome to CrazyDevShare</h2>
+      <v-btn color="primary">click</v-btn>
+    </v-content>
   </v-container>
 </template>
 
 <script>
-
 </script>
 
 <style>
-
 </style>
