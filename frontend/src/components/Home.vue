e53993b26b18a0a4204d7cc6eeba9d3d10660d28
SirawichDev <sirawit0676@gmail.com>
2019-01-19 11:16:23 +0700

    query getUser function from backend with gql

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -1,17 +1,32 @@
 <template>
-<v-app>
-  <v-container class="mt-5">
-    <v-content>
-      <h2>Welcome to CrazyDevShare</h2>
-      <v-btn color="primary">click</v-btn>
-    </v-content>
-  </v-container>
-</v-app>
+  <v-app>
+    <v-container class="mt-5">
+      <v-content>
+        <h2>Welcome to CrazyDevShare</h2>
+        <v-btn color="primary">click</v-btn>
+        <ul v-for="user in getUser" :key="user._id">{{user.username}} - {{user.email}}</ul>
+      </v-content>
+    </v-container>
+  </v-app>
 </template>
 
 <script>
+import {gql} from 'apollo-boost';
 export default {
-  name: 'home'
+  name: 'home',
+  apollo: {
+   getUser :{
+     query: gql`
+      query {
+        getUser{
+          _id
+          username
+          email
+        }
+      }
+     `
+   }
+  }
 }
 </script>
 
