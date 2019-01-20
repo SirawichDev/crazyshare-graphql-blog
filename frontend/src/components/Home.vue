cfadf88a941052bb71280623129b0a6771beeba3
SirawichDev <sirawit0676@gmail.com>
2019-01-20 10:20:16 +0700

    get article with query

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -3,31 +3,44 @@
     <v-container class="mt-5">
       <v-content>
         <h2>Welcome to CrazyDevShare</h2>
-        <v-btn color="primary">click</v-btn>
-        <ul v-for="user in getUser" :key="user._id">{{user.username}} - {{user.email}}</ul>
+        <div v-if="$apollo.loading">Loading...</div>
+        <v-container v-else>
+          <v-flex>
+            <v-carousel v-bind="{'cycle' :true}" interval="3000">
+              <v-carousel-item
+                v-for="article in getArticle"
+                :key="article._id"
+                :src="article.imageUrl"
+              >
+              <h1>{{article.title}}</h1>
+              </v-carousel-item>
+              </v-carousel>
+          </v-flex>
+        </v-container>
       </v-content>
     </v-container>
   </v-app>
 </template>
 
 <script>
-import {gql} from 'apollo-boost';
+import { gql } from "apollo-boost";
 export default {
-  name: 'home',
+  name: "home",
   apollo: {
-   getUser :{
-     query: gql`
-      query {
-        getUser{
-          _id
-          username
-          email
+    getArticle: {
+      query: gql`
+        query {
+          getArticle {
+            _id
+            title
+            imageUrl
+            description
+          }
         }
-      }
-     `
-   }
+      `
+    }
   }
-}
+};
 </script>
 
 <style scoped>
