ede3b1dc0f50ab699dcff593909a4aad17035ded
SirawichDev <sirawit0676@gmail.com>
2019-01-20 14:10:11 +0700

    new design navbar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -9,61 +9,60 @@
     >
 
     </v-navigation-drawer>
-    <v-container >
-    <v-toolbar
-      class="nav"
-      fixed
-      dark
-      color="transparent"
-    >
-      <v-toolbar-side-icon
-        @click="toggle"
-        color="primary"
+    <v-container>
+      <v-toolbar
+        class="nav"
+        fixed
+        dark
+        color="transparent"
       >
-      </v-toolbar-side-icon>
-      <v-toolbar-title class="title">
-        <router-link
-          to="/"
-          tag="span"
-          style="cursor: pointer"
-          id="title"
+        <v-toolbar-side-icon
+          @click="toggle"
+          color="primary"
         >
-          CrazyDev-Share
-        </router-link>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-text-field
-        flex
-        label="Search"
-        class="search"
-        prepend-inner-icon="search"
-        solo-inverted
-        hide-details
-      ></v-text-field>
-      <v-spacer></v-spacer>
-      <v-toolbar-items class="hidden-xs-only">
-        <v-btn
-          class="btn"
-          flat
-          color="black"
-          v-for="item in eachItem"
-          :key="item.title"
-          :to="item.link"
-        >
-          <v-icon
-            class="hidden-sm-only"
-            left
-            color="primary"
-          >{{item.icon}}</v-icon>
-       <h3 id="title">   {{item.title}}</h3>
-        </v-btn>
-      </v-toolbar-items>
-    </v-toolbar>
+        </v-toolbar-side-icon>
+        <v-toolbar-title class="title">
+          <router-link
+            to="/"
+            tag="span"
+            style="cursor: pointer"
+            id="title"
+          >
+            CrazyDev-Share
+          </router-link>
+        </v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-text-field
+          flex
+          label="Search"
+          class="search"
+          prepend-inner-icon="search"
+          solo-inverted
+        ></v-text-field>
+        <v-spacer></v-spacer>
+        <v-toolbar-items class="hidden-xs-only">
+          <v-btn
+            class="btn"
+            flat
+            color="black"
+            v-for="item in eachItem"
+            :key="item.title"
+            :to="item.link"
+          >
+            <v-icon
+              class="hidden-sm-only"
+              left
+              color="primary"
+            >{{item.icon}}</v-icon>
+            <h3 id="title"> {{item.title}}</h3>
+          </v-btn>
+        </v-toolbar-items>
+      </v-toolbar>
     </v-container>
 
-      <transition name="tranpage">
-        <router-view />
-      </transition>
+    <transition name="tranpage">
+      <router-view />
+    </transition>
   </v-app>
 </template>
 
@@ -100,22 +99,20 @@ export default {
 <style scoped lang="scss">
 @import url("https://fonts.googleapis.com/css?family=Concert+One");
 .nav {
-  background-color: aquamarine;
-  border: 1px 1px 1px #444;
-}
+   background-image: linear-gradient(120deg, #84fab0 0%, #8fd3f4 100%);
+   border-radius: 2em;
+clip-path: polygon(0% 15%, 15% 15%, 15% 0%, 85% 0%, 85% 15%, 100% 15%, 100% 85%, 85% 85%, 85% 100%, 15% 100%, 15% 85%, 0% 85%);}
 #title {
   font-family: "Concert One", cursive !important;
   color: black;
   font-weight: 900;
 }
-.search {
-  border-radius: 2em;
-  background-color: black;
-  -webkit-clip-path: polygon(0 0, 91% 0, 100% 100%, 8% 100%);
-  clip-path: polygon(0 0, 91% 0, 100% 100%, 8% 100%);
+v-text-field {
+  color: #444 !important;
+  border: 10px !important;
 }
 .btn {
-  border-radius: 20px;
+  border-radius: 100px;
   color: black;
 }
 .login {
