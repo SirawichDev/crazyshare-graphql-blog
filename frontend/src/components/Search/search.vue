e3dc3b332b3a129da607e5aeb47c265a180caedf
SirawichDev <sirawit0676@gmail.com>
2019-01-20 10:21:43 +0700

    design Search component

---
+++ frontend/src/components/Search/search.vue
@@ -0,0 +1,93 @@
+<template>
+  <form class="search-form">
+    <input
+      type="search"
+      value=""
+      placeholder="Search"
+      class="search-input"
+    >
+    <button
+      type="submit"
+      class="search-button"
+    >
+      <svg class="submit-button">
+        <use
+          xmlns:xlink="http://www.w3.org/1999/xlink"
+          xlink:href="#search"
+        ></use>
+      </svg>
+    </button>
+    <div class="search-option">
+      <div>
+        <input
+          name="type"
+          type="radio"
+          value="type-users"
+          id="type-users"
+        >
+        <label for="type-users">
+          <svg class="edit-pen-title">
+            <use
+              xmlns:xlink="http://www.w3.org/1999/xlink"
+              xlink:href="#user"
+            ></use>
+          </svg>
+          <span>Users</span>
+        </label>
+      </div>
+
+      <div>
+        <input
+          name="type"
+          type="radio"
+          value="type-posts"
+          id="type-posts"
+        >
+        <label for="type-posts">
+          <svg class="edit-pen-title">
+            <use
+              xmlns:xlink="http://www.w3.org/1999/xlink"
+              xlink:href="#post"
+            ></use>
+          </svg>
+          <span>Posts</span>
+        </label>
+      </div>
+      <div>
+        <input
+          name="type"
+          type="radio"
+          value="type-images"
+          id="type-images"
+        >
+        <label for="type-images">
+          <svg class="edit-pen-title">
+            <use
+              xmlns:xlink="http://www.w3.org/1999/xlink"
+              xlink:href="#images"
+            ></use>
+          </svg>
+          <span>Images</span>
+        </label>
+      </div>
+      <div>
+        <input
+          name="type"
+          type="radio"
+          value="type-special"
+          id="type-special"
+          checked=""
+        >
+        <label for="type-special">
+          <svg class="edit-pen-title">
+            <use
+              xmlns:xlink="http://www.w3.org/1999/xlink"
+              xlink:href="#special"
+            ></use>
+          </svg>
+          <span>Special</span>
+        </label>
+      </div>
+    </div>
+  </form>
+</template>
