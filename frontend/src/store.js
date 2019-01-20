cc19bf8acf9e6d1e1bd9226d5a824a149946f1a6
SirawichDev <sirawit0676@gmail.com>
2019-01-20 11:36:48 +0700

    put getArticle query in action of store

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,10 +1,34 @@
+/* eslint-disable no-console */
 import Vue from 'vue';
 import Vuex from 'vuex';
-
+import { gql } from 'apollo-boost';
+import { client } from './main';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
     state: {},
     mutations: {},
-    actions: {}
+    actions: {
+        getArticle: () => {
+            client
+                .query({
+                    query: gql`
+                        query {
+                            getArticle {
+                                _id
+                                title
+                                description
+                                imageUrl
+                            }
+                        }
+                    `
+                })
+                .then(data => {
+                    console.log(data);
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        }
+    }
 });
