de206249e9880a2e6baa7751dba64e542aca99ee
SirawichDev <sirawit0676@gmail.com>
2019-01-21 16:07:26 +0700

    query curentUser

--- backEnd/resolvers/query.js
+++ backEnd/resolvers/query.js
@@ -8,6 +8,15 @@ const Query = {
             });
         return article;
     },
+    currentUser: async (parent, args, ctx, info) => {
+        if (!ctx.currentUser) {
+            return null;
+        }
+        const user = await ctx.User.findOne({
+            username: ctx.currentUser.username
+        }).populate({ path: 'bookmarks', model: 'Article' });
+        return user;
+    },
     getUser: async (parent, args, { User }) => {
         const user = await User.find({}).sort({ joinDate: 'desc' });
         return user;
