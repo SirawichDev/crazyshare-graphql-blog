f6b2e4d06a3f0724d535ec456f368a496297c357
SirawichDev <sirawit0676@gmail.com>
2019-01-21 13:00:37 +0700

    make password encrypt with hash and make avatar when create user

--- backEnd/models/User.js
+++ backEnd/models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const md5 = require('md5');
+const bcrypt = require('bcrypt');
 
 const UserSchema = new mongoose.Schema({
     username: {
@@ -30,5 +32,24 @@ const UserSchema = new mongoose.Schema({
         ref: 'Article'
     }
 });
+UserSchema.pre('save', function(next) {
+    this.avatar = `http://gravatar.com/avatar/${md5(
+        this.username
+    )}?d=identicon`;
+    next();
+});
 
+UserSchema.pre('save', function(next) {
+    if (!this.isModified('password')) {
+        return next();
+    }
+    bcrypt.genSalt(10, (err, salt) => {
+        if (err) return next(err);
+        bcrypt.hash(this.password, salt, (err, hash) => {
+            if (err) return next(err);
+            this.password = hash;
+            next();
+        });
+    });
+});
 module.exports = mongoose.model('User', UserSchema);
