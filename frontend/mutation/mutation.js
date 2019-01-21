29dbaa7d82212242449c1cb288aa6a20d576f92a
SirawichDev <sirawit0676@gmail.com>
2019-01-21 14:34:22 +0700

    add gql of sigin and signup mutation

---
+++ frontend/mutation/mutation.js
@@ -0,0 +1,17 @@
+import { gql } from 'apollo-boost';
+
+export const SIGNUP_USER = gql`
+    mutation($username: String!, $email: String!, $password: String!) {
+        signupUser(username: $username, email: $email, password: $password) {
+            token
+        }
+    }
+`;
+
+export const SIGNIN_USER = gql`
+    mutation($username: String!, $password: String!) {
+        signinUser(username: $username, password: $password) {
+            token
+        }
+    }
+`;
