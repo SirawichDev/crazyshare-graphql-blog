91e7f40ff6976f3bacc859f70f4d32de4f38dea8
SirawichDev <sirawit0676@gmail.com>
2019-01-22 10:42:47 +0700

    Error Alert ui

---
+++ frontend/src/components/Alert/Error/Error.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-card>
+
+    <v-snackbar
+      v-model="snackbar"
+      :bottom="true"
+      :multi-line="mode === 'multi-line'"
+      :vertical="mode === 'vertical'"
+    >
+    <h3>  {{ message.replace('GraphQL', '') }}</h3>
+      <v-btn
+        color="pink"
+        flat
+        @click="snackbar = false"
+      >
+        Close
+      </v-btn>
+    </v-snackbar>
+  </v-card>
+</template>
+<script>
+export default {
+  props: ['message'],
+  data() {
+    return {
+        snackbar: true
+    }
+  }
+};
+</script>
