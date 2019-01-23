e58951575ba323efb65defad1872ad76b2908d7a
SirawichDev <sirawit0676@gmail.com>
2019-01-23 19:36:35 +0700

    route to add article

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -3,6 +3,7 @@ import Router from 'vue-router';
 import Home from './components/Home';
 import SignIn from './components/Auth/Signin';
 import Signup from './components/Auth/Signup';
+import Article from './components/Articles/AddArticle';
 Vue.use(Router);
 
 export default new Router({
@@ -22,6 +23,11 @@ export default new Router({
             path: '/signup',
             name: 'signup',
             component: Signup
+        },
+        {
+            path: '/article/add',
+            name: 'article',
+            component: Article
         }
     ]
 });
