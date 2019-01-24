5a8663c789df33de5925240330e72598ce958d56
SirawichDev <sirawit0676@gmail.com>
2019-01-24 20:17:40 +0700

    added single article page

---
+++ frontend/src/components/Articles/Article.vue
@@ -0,0 +1,13 @@
+<template>
+    <v-container grid-list-xs>
+        <v-layout row wrap>
+            <h1>Article {{articleId}}</h1>
+        </v-layout>
+    </v-container>
+</template>
+<script>
+export default {
+    name: "Article",
+    props: ['articleId']
+}
+</script>
