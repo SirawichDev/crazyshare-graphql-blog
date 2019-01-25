1a2674bca1bf79d21c5b56bf5f2dca4078510535
SirawichDev <sirawit0676@gmail.com>
2019-01-25 09:36:29 +0700

    .

--- frontend/src/components/Articles/Article.vue
+++ frontend/src/components/Articles/Article.vue
@@ -1,13 +1,25 @@
 <template>
-    <v-container grid-list-xs>
+    <v-container grid-list-xs v-if="getSingleArticle">
         <v-layout row wrap>
             <h1>Article {{articleId}}</h1>
         </v-layout>
     </v-container>
 </template>
 <script>
+
+import {GET_SINGLE_ARTICLE} from '../../../query/queries';
 export default {
     name: "Article",
-    props: ['articleId']
+    props: ['articleId'],
+    apollo: {
+        getSingleArticle: {
+            query: GET_SINGLE_ARTICLE,
+            variables(){
+                return {
+                    articleId: this.articleId
+                }
+            }
+        }
+    }
 }
 </script>
